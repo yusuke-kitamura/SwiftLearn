8e1b3b2b2d18682acf63a057e402dc41cd80b01b
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-08-01 12:18:27 +0900

    first commit

--- SwiftLearn/ViewController.swift
+++ SwiftLearn/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func button(_ sender: Any) {
+    }
+    
 
 }
 
